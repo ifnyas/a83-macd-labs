c8eb0fc655ce523679f9d11e4d9ea90617c3485a
Irfan Yas <irfanyasiras@gmail.com>
2019-07-27 11:20:41 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 		<title>Upload Blob</title>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+		/*<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 		<style type="text/css">
 		   body { background-color: #fff; border-top: solid 10px #000;
 			   color: #333; font-size: .85em; margin: 20; padding: 20;
@@ -14,7 +14,7 @@
 		   table { margin-top: 0.75em; }
 		   th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
 		   td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
-		</style>
+		</style>*/
 	</head>
 	
 	<body>
