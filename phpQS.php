d03be8d1b3cfb85931338ec57c08f8fe82f7ab11
Irfan Yas <irfanyasiras@gmail.com>
2019-07-29 14:52:46 +0700

    Update phpQS.php

--- phpQS.php
+++ phpQS.php
@@ -57,7 +57,7 @@ $test = "HelloWorld.txt"
 
 // Create blob client.
 $blobClient = BlobRestProxy::createBlobService($connectionString);
-$fileToUpload = $test;
+$fileToUpload = "HelloWorld.txt";
 if (!isset($_GET["Cleanup"])) {
     // Create container options object.
     $createContainerOptions = new CreateContainerOptions();
@@ -84,7 +84,7 @@ if (!isset($_GET["Cleanup"])) {
         // Create container.
         $blobClient->createContainer($containerName, $createContainerOptions);
         // Getting local file so that we can upload it to Azure
-        $myfile = fopen($fileToUpload, "r") or die("Unable to open file!");
+        $myfile = fopen($fileToUpload, "w") or die("Unable to open file!");
         fclose($myfile);
         
         # Upload file as a block blob
